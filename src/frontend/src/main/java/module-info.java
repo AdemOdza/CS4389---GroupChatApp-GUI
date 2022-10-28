5b305ceb37942f1dbcc17aa0111ed08dbd02164b
AdemOdza <AdemOdza@gmail.com>
2022-10-28 17:28:00 -0500

    Initial graphics

---
+++ src/frontend/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.chatroomgui.gui_chatroom {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.chatroomgui.gui_chatroom to javafx.fxml;
+    exports com.chatroomgui.gui_chatroom;
+}
