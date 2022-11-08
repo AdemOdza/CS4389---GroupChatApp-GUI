e785bf8f1553d9405fb836f5d98652e022e25be5
emart870 <72898341+emart870@users.noreply.github.com>
2022-11-08 08:37:32 -0600

    Updated HelloApplication and HelloController

--- src/frontend/src/main/java/com/chatroomgui/gui_chatroom/HelloController.java
+++ src/frontend/src/main/java/com/chatroomgui/gui_chatroom/HelloController.java
@@ -1,14 +1,28 @@
 package com.chatroomgui.gui_chatroom;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class HelloController {
+    @FXML private Label welcomeText;
+    @FXML private TextField passwordTextField;
+    @FXML private TextField usernameTextField;
+    
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() 
+    {
+        String password = passwordTextField.getText();
+        String username = usernameTextField.getText();
+        if(!username.equals("username") || !password.equals("password"))
+        {
+            welcomeText.setText("Incorrect username and/or password");
+        }
+        else
+        {
+            welcomeText.setText("You're good to go");
+        }
     }
 }
